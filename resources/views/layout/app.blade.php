98f7da6365d5c381d82993b2b86fcb1f8c6240cf
andreagavardi <andreagavardi89@gmail.com>
2021-07-08 17:42:01 +0200

    strutturato footer

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,7 +11,6 @@
 </head>
 
 <body>
-
     <header id="site_header">
 
         <div class="top">
@@ -52,7 +51,66 @@
         @yield('main_content')
     </main>
 
-    <footer id="site_footer"></footer>
+    <footer id="site_footer">
+
+        <div class="container">
+            <div class="column">
+                <div class="footer_menu">
+                    <h3> dc comics</h3>
+                    <ul>
+                        <li><a href="#">Chracters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                </div>
+                <div class="footer_menu">
+                    <h3>shop</h3>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <div class="footer_menu">
+                    <h3>dc</h3>
+                    <ul>
+                        <li><a href="#">Term Of Use</a></li>
+                        <li><a href="#">Privacy policy(NEW)</a></li>
+                        <li><a href="#">Ad Choice</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscription</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+
+                    </ul>
+
+                </div>
+            </div>
+            <div class="column">
+                <div class="footer_menu">
+                    <h3>sites</h3>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Univers</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 </body>
 
---
+++ resources/views/partials/_header.php
@@ -0,0 +1,35 @@
+ <header id="site_header">
+
+     <div class="top">
+         <div class="container">
+             <span>DC POWER VISA</span>
+             <span> ADDITIONAL DC SITES</span>
+         </div>
+
+     </div>
+     <div class="container">
+         <nav id="navbar">
+             <div class="logo">
+                 <img src="../img/dc-logo.png" alt="logo dc">
+             </div>
+             <div class="nav_links">
+                 <a href="#">characters</a>
+                 <a href="#" class="active">comics</a>
+                 <a href="#">movies</a>
+                 <a href="#">tv</a>
+                 <a href="#">games</a>
+                 <a href="#">collectibles</a>
+                 <a href="#">videos</a>
+                 <a href="#">fans</a>
+                 <a href="#">news</a>
+                 <a href="#">shop</a>
+             </div>
+             <div class="nav_search">
+                 <input type="text" name="search" id="search" placeholder="Search">
+                 <i class="fas fa-search fa-sm fa-fw"></i>
+             </div>
+         </nav>
+     </div>
+
+
+ </header>
