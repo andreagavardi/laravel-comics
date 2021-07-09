b930a4e3b97c4de5c4757f11f56401756b845926
andreagavardi <andreagavardi89@gmail.com>
2021-07-09 16:30:37 +0200

    Inserito thumb e title in /comics.show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,18 +1,40 @@
 @extends('layout.app')
 @section('title','Comic Page')
+
 @section('main_content')
 <div class="jumbotron">
-    <div class="series">
-        <h2>current series</h2>
+    <div class="container_sm">
+
+        <div class="thumb">
+            <img src="{{$comic['thumb']}}" alt="">
+            <a href="{{route('comics')}}">View Gallery</a>
+        </div>
     </div>
 </div>
-<div class="comics_series">
-    <div class="container">
+<div class="banner"></div>
+<div class="comic_details">
 
-        <div class="comic">
-            <img src="{{$comic['thumb']}}" alt="">
-            <h4>{{$comic['series']}}</h4>
+    <div class="container_sm">
+        <div class="col_left">
+            <h1>{{$comic['title']}}</h1>
+            <div class="price_details">
+                <div class="sell">
+                    <div class="price">
+                        <span class="yellow">U.S. Price:</span>
+                        <span> {{$comic['price']}}</span>
+                    </div>
+                    <div><span class="yellow">AVAILABLE</span></div>
+                </div>
+                <div class="check_availability">
+                    <a href="#">Check Availability</a>
+                </div>
+
+            </div>
         </div>
+        <div class="col_right">
+
+        </div>
+
 
     </div>
 </div>
